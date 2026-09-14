cd1bf09a3465a153f66f6464d9098ba38459d845
vovas-coder <volodyapyrch2012@gmail.com>
2026-09-14 12:24:18 +0300

    lol

---
+++ js.js
@@ -0,0 +1,26 @@
+// result
+const result = 5 + 5 + "5";
+console.log(result)
+console.log(typeof result)
+
+// email
+
+const email = "volodyapurch2012@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length)
+
+// victor
+
+const num1 = "My ";
+const num2 = "Name ";
+const num3 = "Is ";
+
+let fullName = num1 + num2 + num3;
+fullName = fullName + 'Victor';
+console.log(fullName)
+
+
+// pay
+let userName = prompt("type your name")
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} грн`)
